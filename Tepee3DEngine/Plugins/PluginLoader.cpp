7bec5e6e412f8af1dee7ed38477dd9dfef7645e7
pubert_c <picque_g@virtual-machine.(none)>
2013-04-14 01:13:43 +0900

    Adding pluginloader (still as to instanciate plugins)

--- Tepee3DEngine/Plugins/PluginLoader.cpp
+++ Tepee3DEngine/Plugins/PluginLoader.cpp
@@ -21,6 +21,34 @@
  * case an instance of the given plugin is saved in a list.
  */
 
+Plugins::PluginLoader*   Plugins::PluginLoader::instance = NULL;
+
+Plugins::PluginLoader::PluginLoader(QObject *parent) : QObject(parent)
+{
+    // CONNECT TO DATABASE SERVICE
+    Services::ServicesManager::connectObjectToServices(this);
+    qDebug() << "Connection PluginLoader to services";
+
+    // DATABASE CALLBACKS
+    this->pFunc[PLUGIN_LOADED] = &Plugins::PluginLoader::loadPluginsCallback;
+    this->pFunc[GENERIC_RESULT] = &Plugins::PluginLoader::genericResultCallback;
+
+}
+
+
+Plugins::PluginLoader::~PluginLoader()
+{
+    Services::ServicesManager::disconnectObjectFromServices(this);
+}
+
+
+Plugins::PluginLoader*   Plugins::PluginLoader::getInstance(QObject *parent)
+{
+    if (Plugins::PluginLoader::instance == NULL)
+        Plugins::PluginLoader::instance = new Plugins::PluginLoader(parent);
+    return Plugins::PluginLoader::instance;
+}
+
 void    Plugins::PluginLoader::loadWidgetPlugins()
 {
     QDir    pluginsDir = QCoreApplication::applicationDirPath();
@@ -68,3 +96,86 @@ QList<Plugins::PluginBase*>  Plugins::PluginLoader::getWidgetPlugins()
 }
 
 QList<Plugins::PluginBase *> Plugins::PluginLoader::widgetPlugins = QList<Plugins::PluginBase *>();
+
+void    Plugins::PluginLoader::receiveResultFromSQLQuery(QList<QSqlRecord> result, int id, void *data)
+{
+    (this->*this->pFunc[id])(result);
+}
+
+void    Plugins::PluginLoader::genericResultCallback(QList<QSqlRecord> result)
+{
+    Q_UNUSED(result);
+}
+
+void    Plugins::PluginLoader::loadPluginsCallback(QList<QSqlRecord> result)
+{
+    qDebug() << "restoreRoomsCallback";
+    if (result.size() > 1) // FIRST RECORD IS STATUS COUNT OF RECORDINGS
+    {
+        result.pop_front();
+        foreach (QSqlRecord rsecord, result)
+        {
+            // Create a new plugin and insert in room manager.
+        }
+    }
+}
+
+void                   Plugins::PluginLoader::loadAllPluginForRoom(Room::RoomBase &room)
+{
+    QString request;
+
+    request = "SELECT widgetImpl.idWidgetImpl, widgetImpl.idRoom, room.name, widgetImpl.idUser, widgetImpl.idWidget, widgetImpl.posX, widgetImpl.posY, widgetImpl.posZ, widgetImpl.scaleX, widgetImpl.scaleY, widgetImpl.scaleZ, widget.name, widget.description, widget.localpath FROM widget, widgetImpl, room WHERE ";
+    request += "widgetImpl.idwidget = widget.idWidget";
+    request += " AND ";
+    request += "widgetImpl.idRoom = room.idRoom AND room.name = \"" + room.getRoomName() + "\""; // TEMPORARY! Should be changed in the future
+    request += ";";
+
+    emit executeSQLQuery(request, this, 1, DB_NAME, NULL);
+}
+
+void                   Plugins::PluginLoader::loadAllPluginForUser(int &idUser)
+{
+    QString request;
+
+    request = "SELECT widgetImpl.idWidgetImpl, widgetImpl.idRoom, room.name, widgetImpl.idUser, widgetImpl.idWidget, widgetImpl.posX, widgetImpl.posY, widgetImpl.posZ, widgetImpl.scaleX, widgetImpl.scaleY, widgetImpl.scaleZ, widget.name, widget.description, widget.localpath FROM widget, widgetImpl, room WHERE ";
+    request += "widgetImpl.idwidget = widget.idWidget";
+    request += " AND ";
+    request += " widgetImpl.idUser = " + QString::number(idUser);
+    request += ";";
+
+    emit  executeSQLQuery(request, this, 1, DB_NAME, NULL);
+}
+
+void                   Plugins::PluginLoader::addPluginImpl(PluginBase &plugin, Room::RoomBase &room, int &idUser, QVector3D &scale, QVector3D &pos)
+{
+    QString request;
+
+    request = "INSERT INTO widgetImpl (idRoom, idWidget, idUser, posX, posY, posZ, scaleX, scaleY, scaleZ) VALUES (";
+    request +=  1 + ", "; // TEMPORARY! Should be changed in the future
+    request +=  1 + ", "; // TEMPORARY! Should be changed in the future
+    request +=  idUser + ", ";
+    request +=  QString::number(pos.x()) + ", ";
+    request +=  QString::number(pos.y()) + ", ";
+    request +=  QString::number(pos.z()) + ", ";
+    request +=  QString::number(scale.x()) + ", ";
+    request +=  QString::number(scale.y()) + ", ";
+    request +=  QString::number(scale.z()) + ")";
+    request += ";";
+
+    emit executeSQLQuery(request, this, 1, DB_NAME, NULL);
+}
+
+void                   Plugins::PluginLoader::addNewPlugin(PluginBase &plugin)
+{
+    QString request;
+
+    request = "INSERT INTO widget (name, description, localpath, minimalage) VALUES (\"";
+    request += plugin.getPluginName() + "\", ";
+    request += plugin.getPluginDescription() + "\", \"";
+    request += plugin.getRoomPluginQmlFile() + "\", \"";
+    request += 3 + "\")"; // TEMPORARY! Should be changed in the future
+    request += ";";
+
+    emit  executeSQLQuery(request, this, 1, DB_NAME, NULL);
+}
+
--- Tepee3DEngine/Plugins/PluginLoader.h
+++ Tepee3DEngine/Plugins/PluginLoader.h
@@ -6,22 +6,50 @@
 #include <QPluginLoader>
 #include <QQmlExtensionPlugin>
 #include <QDir>
+#include <DatabaseServiceUserInterface.h>
 #include "PluginInterface.h"
 #include "PluginBase.h"
+#include <PluginManager.h>
+#include <RoomBase.h>
 
 #define WIDGET_PLUGIN_DIRECTORY "libraries/widget_libraries"
+
+#define PLUGIN_LOADED 1
+#define DB_NAME "Tepee3D.sql"
 
 namespace Plugins
 {
 
-class PluginLoader
+class PluginLoader :  public QObject, public Services::DatabaseServiceUserInterface
 {
-private :
-    PluginLoader(); // PRIVATE SO THAT IT CANNOT  BE CALLED
-    static QList<PluginBase *>     widgetPlugins; //LIST CONTAINING ALL THE PLUGINS
-public :
-    static void                loadWidgetPlugins(); //TRIES TO LOAD EVERY PLUGIN IN DIR AND RETURN FALSE IF ERROR OCCURS
-    static QList<PluginBase *> getWidgetPlugins(); // RETURN LIST CONTAINING ALL THE LOADED PLUGINS
+    Q_OBJECT
+    Q_INTERFACES(Services::DatabaseServiceUserInterface)
+
+    QHash<int, void (PluginLoader::*)(QList<QSqlRecord> result)>   pFunc;
+    private :
+      PluginLoader(QObject *parent = 0); // PRIVATE SO THAT IT CANNOT  BE CALLED
+      static QList<PluginBase *>     widgetPlugins; //LIST CONTAINING ALL THE PLUGINS
+
+       static      PluginLoader*     instance;
+    public :
+      ~PluginLoader();
+      static void                loadWidgetPlugins(); //TRIES TO LOAD EVERY PLUGIN IN DIR AND RETURN FALSE IF ERROR OCCURS
+      static QList<PluginBase *> getWidgetPlugins(); // RETURN LIST CONTAINING ALL THE LOADED PLUGINS
+
+      void                       genericResultCallback(QList<QSqlRecord> result);
+      void                       loadPluginsCallback(QList<QSqlRecord> result);
+    public :
+          static PluginLoader*   getInstance(QObject *parent = 0);
+
+          void                   loadAllPluginForRoom(Room::RoomBase &room);
+          void                   loadAllPluginForUser(int &idUser); // Change int by user class later
+          void                   addPluginImpl(PluginBase &plugin, Room::RoomBase &room, int &idUser, QVector3D &scale, QVector3D &pos);
+          void                   addNewPlugin(PluginBase &plugin);
+
+          void                   receiveResultFromSQLQuery(QList<QSqlRecord> result, int id, void *data);
+    signals :
+      void                       executeSQLQuery(const QString &query, QObject *sender, int id, const QString &dbName, void *data);
+
 };
 
 }
