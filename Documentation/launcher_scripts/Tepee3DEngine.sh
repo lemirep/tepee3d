664ad1e6e6462cc6e73529c23fda8b3f8fc78ad6
Paul Lemire <paul.lemire@epitech.eu>
2013-06-18 11:30:34 +0200

    Added Bash Script to set paths and launch Tepee3D executable in installers for linux

---
+++ Documentation/launcher_scripts/Tepee3DEngine.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export LD_LIBRARY_PATH=`pwd`/Qt_Libraries
+export QML_IMPORT_PATH=`pwd`/Qt_Libraries/qml
+export QML2_IMPORT_PATH=`pwd`/Qt_Libraries/qml
+export QT_QPA_PLATFORM_PLUGIN_PATH=`pwd`/Qt_Libraries/plugins/platforms
+`pwd`/Tepee3DEngine
