673b4b7a62dcfc38eb5095bd2fc0e71ba05f983c
Paul Lemire <paul.lemire@epitech.eu>
2013-06-26 22:17:18 +0200

    Extract Qt and System files from the code coverage

--- UnitTests/gcov_files.py
+++ UnitTests/gcov_files.py
@@ -5,19 +5,6 @@ import shutil
 import sys
 import os
 
-def browse_source_dir(entry):
-#    print "Entry " + entry
-    if os.path.isdir(entry):
-        gcov_source_file(entry)
-
-def gcov_source_file(folder_entry):
-    try:
-        print "gcov -o " + folder_entry + " *.cpp"
-        return_value = subprocess.call("gcov -o " + folder_entry + " *.cpp", shell=True)
-        print "Returned {" + str(return_value) + "}"
-    except OSError as ex:
-        print ex
-        
 def main(args):
     if len(args) <= 1:
         print "./gcov_files source_dir source_dir ..."
@@ -25,7 +12,12 @@ def main(args):
         try:
             arg = args[0]
             for path in args:
+                print "lcov --base-directory " + path + " --directory " + path + " --no-external -c -o " + path + "/coverage.info"
                 return_value = subprocess.call("lcov --base-directory " + path + " --directory " + path + " -c -o " + path + "/coverage.info", shell=True)
+                print "Returned {" + str(return_value) + "}"
+                return_value = subprocess.call("lcov -r " + path + "/coverage.info \"*/include/*\" -o " + path + "/coverage.info", shell=True)
+                print "Returned {" + str(return_value) + "}"
+                return_value = subprocess.call("lcov -r " + path + "/coverage.info \"*moc_*\" -o " + path + "/coverage.info", shell=True)
                 print "Returned {" + str(return_value) + "}"
                 if not path is arg:
                     subprocess.call("cat " + path + "/coverage.info >> " + arg + "/coverage.info", shell=True)
