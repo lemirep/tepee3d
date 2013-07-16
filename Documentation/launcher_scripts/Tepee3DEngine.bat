f0eb85dab36685366a59b3545943b7150af6cf95
Paul Lemire <paul.lemire@epitech.eu>
2013-07-16 17:57:25 +0200

    Added launcher script for windows installer

---
+++ Documentation/launcher_scripts/Tepee3DEngine.bat
@@ -0,0 +1,6 @@
+SET PATH=%cd%\Qt_Libraries;%PATH%
+SET QML_IMPORT_PATH=%cd%\Qt_Libraries\qml
+SET QML2_IMPORT_PATH=%cd%\Qt_Libraries\qml
+SET QT_QPA_PLATFORM_PLUGIN_PATH=%cd%\Qt_Libraries\platforms
+
+Tepee3DEngine.exe
