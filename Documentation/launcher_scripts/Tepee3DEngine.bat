c03d26d073faa8bc37938373068fa8cdf7b9e20c
Paul Lemire <paul.lemire@epitech.eu>
2013-09-11 18:10:15 +0200

    Configure .pro for log under windows and modifier windows launcher script to include QtPlugins

--- Documentation/launcher_scripts/Tepee3DEngine.bat
+++ Documentation/launcher_scripts/Tepee3DEngine.bat
@@ -1,6 +1,7 @@
 SET PATH=%cd%\Qt_Libraries;%PATH%
 SET QML_IMPORT_PATH=%cd%\Qt_Libraries\qml
 SET QML2_IMPORT_PATH=%cd%\Qt_Libraries\qml
-SET QT_QPA_PLATFORM_PLUGIN_PATH=%cd%\Qt_Libraries\platforms
+SET QT_QPA_PLATFORM_PLUGIN_PATH=%cd%\Qt_Libraries\plugins\platforms
+SET QT_PLUGIN_PATH=%cd%\Qt_Libraries\plugins
 
 Tepee3DEngine.exe
