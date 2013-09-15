8975d994752d6de8b90fbc09b7a4c77b71e1b382
Paul Lemire <paul.lemire@epitech.eu>
2013-09-15 15:23:46 -0400

    Updated the doc, images should now be included

--- Documentation/launcher_scripts/Tepee3DEngine.sh
+++ Documentation/launcher_scripts/Tepee3DEngine.sh
@@ -2,5 +2,6 @@
 export LD_LIBRARY_PATH=`pwd`/Qt_Libraries
 export QML_IMPORT_PATH=`pwd`/Qt_Libraries/qml
 export QML2_IMPORT_PATH=`pwd`/Qt_Libraries/qml
-export QT_QPA_PLATFORM_PLUGIN_PATH=`pwd`/Qt_Libraries/platforms
+export QT_QPA_PLATFORM_PLUGIN_PATH=`pwd`/Qt_Libraries/plugins/platforms
+export QT_PLUGIN_PATH=`pwd`/Qt_Libraries/plugins
 `pwd`/Tepee3DEngine
